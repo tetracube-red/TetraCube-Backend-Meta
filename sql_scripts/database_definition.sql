ea9de551ccc1c626452fe0c4c1842b5565252ecb
dave <davide.dcube@gmail.com>
2022-12-18 22:28:58 +0100

    [🧈] Added switcher related tables, upgraded framework

--- sql_scripts/database_definition.sql
+++ sql_scripts/database_definition.sql
@@ -1,12 +1,13 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE
+    EXTENSION IF NOT EXISTS "uuid-ossp";
 
-create table if not exists houses
+create table houses
 (
     id   uuid primary key    not null,
     name varchar(255) unique not null
-    );
+);
 
-create table if not exists authentication_tokens
+create table authentication_tokens
 (
     id          uuid primary key    not null,
     token       varchar(255) unique not null,
@@ -15,24 +16,24 @@ create table if not exists authentication_tokens
     is_valid    bool                not null default true,
     in_use      boolean             not null default false,
     id_house    uuid                not null references houses (id)
-    );
+);
 
-create table if not exists accounts
+create table accounts
 (
     id                      uuid primary key    not null,
     name                    varchar(255) unique not null,
     id_house                uuid                not null references houses (id),
     id_authentication_token uuid                not null references authentication_tokens (id)
-    );
+);
 
-create table if not exists environments
+create table environments
 (
     id       uuid primary key not null,
     name     varchar(255)     not null,
     house_id uuid             not null references houses (id)
-    );
+);
 
-create table if not exists devices
+create table devices
 (
     id             uuid primary key not null,
     color_code     varchar(255),
@@ -43,4 +44,28 @@ create table if not exists devices
     device_type    varchar(100)     not null,
     environment_id uuid             not null,
     house_id       uuid             not null references houses (id)
-    );
+);
+
+create table switchers
+(
+    id            uuid primary key not null,
+    command_topic varchar(255)     not null unique,
+    device_id     uuid references devices (id)
+);
+
+create table switchers_actions
+(
+    id           uuid primary key not null,
+    logic_state  varchar(255)     not null,
+    mqtt_command varchar(255)     not null,
+    name         varchar(255)     not null,
+    switcher_id  uuid             not null
+);
+
+create table switchers_history
+(
+    id          uuid primary key not null,
+    logic_state varchar(255)     not null,
+    stored_at   timestamp        not null,
+    switcher_id uuid             not null
+);
